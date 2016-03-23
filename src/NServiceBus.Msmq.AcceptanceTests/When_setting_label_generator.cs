4fdd0bf52d9500d4cf994ed526a4cf796bf705e8
Kyle Baley <kyle@baley.org>
2016-03-23 14:32:15 -0500

    Removed unused generator property in When_setting_label_generator

--- src/NServiceBus.Msmq.AcceptanceTests/When_setting_label_generator.cs
+++ src/NServiceBus.Msmq.AcceptanceTests/When_setting_label_generator.cs
@@ -4,15 +4,12 @@
     using System.Collections.Generic;
     using System.Messaging;
     using System.Threading.Tasks;
-    using Features;
-    using NServiceBus.AcceptanceTesting;
-    using NServiceBus.Settings;
+    using AcceptanceTesting;
     using NUnit.Framework;
+    using Settings;
 
     public class When_setting_label_generator : NServiceBusAcceptanceTest
     {
-        const string auditQueue = @".\private$\labelAuditQueue";
-
         [Test]
         public async Task Should_receive_the_message_and_label()
         {
@@ -49,57 +46,24 @@
                 return null;
             }
             using (var queue = new MessageQueue(auditQueue))
-            using (var message = queue.Receive(TimeSpan.FromSeconds(5)))
             {
-                return message?.Label;
+                using (var message = queue.Receive(TimeSpan.FromSeconds(5)))
+                {
+                    return message?.Label;
+                }
             }
         }
+
+        const string auditQueue = @".\private$\labelAuditQueue";
 
         public class Context : ScenarioContext
         {
             public bool WasCalled { get; set; }
             public Guid Id { get; set; }
-
-            public bool GeneratorWasCalled { get; set; }
-        }
-
-        class StartHandler : Feature
-        {
-            protected override void Setup(FeatureConfigurationContext context)
-            {
-                context.Container.ConfigureComponent<StartHandlerTask>(DependencyLifecycle.SingleInstance);
-                context.RegisterStartupTask(b => b.Build<StartHandlerTask>());
-            }
-        }
-
-        class StartHandlerTask : FeatureStartupTask
-        {
-            Context context;
-            ReadOnlySettings settings;
-
-            public StartHandlerTask(Context context, ReadOnlySettings settings)
-            {
-                this.context = context;
-                this.settings = settings;
-            }
-
-            protected override Task OnStart(IMessageSession session)
-            {
-                context.GeneratorWasCalled = settings.Get<bool>("GeneratorWasCalled");
-                return Task.FromResult(0);
-            }
-
-            protected override Task OnStop(IMessageSession session)
-            {
-                return Task.FromResult(0);
-            }
         }
 
         public class Endpoint : EndpointConfigurationBuilder, IWantToRunBeforeConfigurationIsFinalized
         {
-            static bool initialized;
-            bool generatorWasCalled;
-
             public Endpoint()
             {
                 if (initialized)
@@ -109,7 +73,6 @@
                 initialized = true;
                 EndpointSetup<DefaultServer>(c =>
                 {
-                    c.EnableFeature<StartHandler>();
                     c.AuditProcessedMessagesTo("labelAuditQueue");
                     c.UseTransport<MsmqTransport>().ApplyLabelToMessages(GetMessageLabel);
                 });
@@ -117,16 +80,16 @@
 
             static Context Context { get; set; }
 
+            public void Run(SettingsHolder config)
+            {
+            }
+
             string GetMessageLabel(IReadOnlyDictionary<string, string> headers)
             {
-                generatorWasCalled = true;
                 return "MyLabel";
             }
 
-            public void Run(SettingsHolder config)
-            {
-                config.Set("GeneratorWasCalled", generatorWasCalled);
-            }
+            static bool initialized;
         }
 
         [Serializable]
@@ -142,7 +105,9 @@
             public Task Handle(MyMessage message, IMessageHandlerContext context)
             {
                 if (Context.Id != message.Id)
+                {
                     return Task.FromResult(0);
+                }
 
                 Context.WasCalled = true;
 
@@ -150,6 +115,4 @@
             }
         }
     }
-
-
 }
